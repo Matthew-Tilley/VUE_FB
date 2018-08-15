7b95d8ba2c60b60a0af1cf2ef66b27d0bf16cfe1
Matt Tilley <mtilley255@gmail.com>
2018-08-15 14:09:16 -0500

    Added .zip file downloaded from instructor's website.

--- collex/src/App.vue
+++ collex/src/App.vue
@@ -7,9 +7,29 @@
 
 <script>
 //import HelloWorld from './components/HelloWorld'
+import Firebase from 'firebase'
+
+let config = {
+
+    apiKey: "AIzaSyBZyil-JrhViIeivyyEp6UhWfHL2ReVSCE",
+    authDomain: "collex-e1ab3.firebaseapp.com",
+    databaseURL: "https://collex-e1ab3.firebaseio.com",
+    projectId: "collex-e1ab3",
+    storageBucket: "collex-e1ab3.appspot.com",
+    messagingSenderId: "972126320719"
+
+}
+
+let app = Firebase.initializeApp(config);
+let db = app.database();
+let ietmsRef = db.ref("items");
 
 export default {
   name: 'App',
+
+  firebase: {
+    items: itemsRef,
+  }
   
 
 }
