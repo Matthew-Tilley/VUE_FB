41df6df65278271fa2ef17707912133f9fa8018c
Matt Tilley <mtilley255@gmail.com>
2018-08-29 13:15:46 -0500

    Added what I 'think' example will be, let's see how close I am to the right answer.

--- collex/src/App.vue
+++ collex/src/App.vue
@@ -2,7 +2,8 @@
   <div id="app1">
 
     <div v-for="item in items" :key="item.id">
-      
+
+      <img v-bind:src="/static/images/{{item.imgname}}.jpg"
       <a v-bind:href="item.pageurl">
       <h1>{{item.title}}</h1>
       <h2>{{item.author}}</h2>
